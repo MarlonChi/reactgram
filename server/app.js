aaf254257780b2c0e32399166ccfdee58da5e298
MarlonChi <marlonchiodelli@hotmail.com>
2023-06-21 11:18:54 -0300

    :wrench: BD connection

--- server/app.js
+++ server/app.js
@@ -12,6 +12,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// solve cors
+app.use(cors({ credentials: true, origin: "http://127.0.0.1:5173" }));
+
+// upload directory
+app.use("/src/uploads", express.static(path.join(__dirname, "/src/uploads")));
+
+// DB connection
+require("./src/config/db.js");
+
 // routes
 const router = require("./src/routes/Router.js");
 app.use(router);
