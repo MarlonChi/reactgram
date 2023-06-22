eae156fc1e154062a46859b62e1bf840a324fc8b
MarlonChi <marlonchiodelli@hotmail.com>
2023-06-22 20:33:41 -0300

    :construction: get user photos, get photo by id and update a photo

--- server/src/routes/PhotoRoutes.js
+++ server/src/routes/PhotoRoutes.js
@@ -5,9 +5,15 @@ const {
   insertPhoto,
   deletePhoto,
   getAllPhotos,
+  getUserPhotos,
+  getPhotoById,
+  updatePhoto,
 } = require("../controllers/PhotoController");
 
-const { photoInsertValidation } = require("../middlewares/photoValidation");
+const {
+  photoInsertValidation,
+  photoUpadateValidation,
+} = require("../middlewares/photoValidation");
 const authGuard = require("../middlewares/authGuard");
 const validate = require("../middlewares/handleValidation");
 const { imageUpload } = require("../middlewares/imageUpload");
@@ -22,5 +28,8 @@ router.post(
 );
 router.delete("/:id", authGuard, deletePhoto);
 router.get("/", authGuard, getAllPhotos);
+router.get("/user/:id", authGuard, getUserPhotos);
+router.get("/:id", authGuard, getPhotoById);
+router.put("/:id", authGuard, photoUpadateValidation, updatePhoto);
 
 module.exports = router;
