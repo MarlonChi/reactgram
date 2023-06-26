41b636ef0708d0f41191a250665b7f1943fdb572
MarlonChi <marlonchiodelli@hotmail.com>
2023-06-26 10:34:39 -0300

    :construction: User Sign In

--- web/src/pages/Auth/Login.jsx
+++ web/src/pages/Auth/Login.jsx
@@ -1,12 +1,60 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+
+import { login, reset } from "../../slices/authSlice";
+
+import { Message } from "../../components/Message/Message";
 
 import "./Auth.css";
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+
+  const { loading, error } = useSelector((state) => state.auth);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const user = {
+      email,
+      password,
+    };
+
+    dispatch(login(user));
+  };
+
+  useEffect(() => {
+    dispatch(reset());
+  }, [dispatch]);
+
   return (
     <div id="Login">
       <h2>ReactGram</h2>
-      <p className="subtitle">Entre para ver as fotos dos seus amigos.</p>
+      <p className="subtitle">Faça login para ver o que há de novo.</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email || ""}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password || ""}
+        />
+        {!loading && <input type="submit" placeholder="Entrar" />}
+        {loading && <input type="submit" placeholder="Aguarde..." disabled />}
+        {error && <Message message={error} type="error" />}
+      </form>
+      <p>
+        Não tem uma conta? <Link to="/register">Clique aqui</Link>
+      </p>
     </div>
   );
 };
