d9d3dbfe0ee4815d1dd6aef0fe0186673837a56b
MarlonChi <marlonchiodelli@hotmail.com>
2023-06-29 10:52:31 -0300

    :construction: Footer update

--- web/src/components/Footer/Footer.jsx
+++ web/src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import "./Footer.css";
 export const Footer = () => {
   return (
     <footer id="footer">
-      <p>ReactGram &copy; 2022</p>
+      <p>ReactGram &copy; 2023</p>
     </footer>
   );
 };
